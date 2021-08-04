11825cd7ab7e45db8aaca98406901360ab151989
naari3 <naari.named@gmail.com>
2021-08-05 04:29:10 +0900

    Focus to client side mod

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,14 +1,19 @@
 package net.fabricmc.example;
 
-import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class ExampleMod implements ModInitializer {
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+import net.fabricmc.api.ClientModInitializer;
 
-		System.out.println("Hello Fabric world!");
-	}
+public class ExampleMod implements ClientModInitializer {
+    public static final Logger logger = LogManager.getLogger("villagertradinglist");
+
+    @Override
+    public void onInitializeClient() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        logger.info("Hello Fabric world!");
+    }
 }
