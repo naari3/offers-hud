71d7b5dfd21a43d028d17cfa61acb5e7e6362555
naari3 <naari.named@gmail.com>
2021-08-05 05:28:45 +0900

    Listing to console

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,9 +1,17 @@
 package net.fabricmc.example;
 
+import java.util.Objects;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult.Type;
 
 public class ExampleMod implements ClientModInitializer {
     public static final Logger logger = LogManager.getLogger("villagertradinglist");
@@ -15,5 +23,22 @@ public class ExampleMod implements ClientModInitializer {
         // Proceed with mild caution.
 
         logger.info("Hello Fabric world!");
+        ClientTickEvents.END_CLIENT_TICK.register(e -> {
+            var mc = MinecraftClient.getInstance();
+            var crosshairTarget = mc.crosshairTarget;
+
+            if (Objects.isNull(crosshairTarget) || crosshairTarget.getType() != Type.ENTITY) {
+                return;
+            }
+
+            var entityHit = (EntityHitResult) crosshairTarget;
+            var entity = entityHit.getEntity();
+            if (entity.getType() != EntityType.VILLAGER) { // TODO: Support MerchantEntity
+                return;
+            }
+
+            var villager = (VillagerEntity) entity;
+            villager.getOffers().forEach(o -> logger.info(o.getSellItem().getItem()));
+        });
     }
 }
