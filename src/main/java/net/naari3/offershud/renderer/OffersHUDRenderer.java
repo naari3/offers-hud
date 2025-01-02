fa33c9b7aea8acb0a0fc9b26035ce014405db0c4
naari3 <naari.named@gmail.com>
2025-01-02 22:06:57 +0900

    Set actives to 1.21.4

--- src/main/java/net/naari3/offershud/renderer/OffersHUDRenderer.java
+++ src/main/java/net/naari3/offershud/renderer/OffersHUDRenderer.java
@@ -22,17 +22,17 @@ import net.naari3.offershud.config.ModConfig;
 
 public class OffersHUDRenderer implements HudRenderCallback {
     private static final Identifier TEXTURE =
-        /*? if <1.21 {*/ new Identifier(OffersHUD.MODID, "textures/gui/container/villager2.png") /*?} else {*/
-        /*Identifier.of(OffersHUD.MODID, "textures/gui/container/villager2.png")
-        *//*?}*/;
+        /*? if <1.21 {*/ /*new Identifier(OffersHUD.MODID, "textures/gui/container/villager2.png") *//*?} else {*/
+        Identifier.of(OffersHUD.MODID, "textures/gui/container/villager2.png")
+        /*?}*/;
 
 
     /*? if >=1.21 {*/
-    /*@Override
+    @Override
     public void onHudRender(DrawContext context, RenderTickCounter tickCounter) {
-    *//*?} else {*/
-     public void onHudRender(DrawContext context, float tickDelta) {
-    /*?}*/
+    /*?} else {*/
+     /*public void onHudRender(DrawContext context, float tickDelta) {
+    *//*?}*/
         var config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
 
         if (!config.enabled)
@@ -55,7 +55,7 @@ public class OffersHUDRenderer implements HudRenderCallback {
         modelMatrices.push();
         modelMatrices.scale(config.scale, config.scale, 1.0f);
         //? if <1.21.3
-        RenderSystem.applyModelViewMatrix();
+        /*RenderSystem.applyModelViewMatrix();*/
 
         MerchantInfo.getInfo().getLastId().ifPresent(lastId -> {
             var offers = MerchantInfo.getInfo().getOffers();
@@ -67,69 +67,69 @@ public class OffersHUDRenderer implements HudRenderCallback {
 
                 var firstBuy =
                         /*? if >=1.20.6 {*/
-                        /* offer.getDisplayedFirstBuyItem().copy() */
+                         offer.getDisplayedFirstBuyItem().copy() 
                         /*?} else {*/
-                        offer.getAdjustedFirstBuyItem().copy()
-                        /*?}*/;
+                        /*offer.getAdjustedFirstBuyItem().copy()
+                        *//*?}*/;
                 var secondBuy =
                         /*? if >=1.20.6 {*/
-                        /* offer.getDisplayedSecondBuyItem().copy() */
+                         offer.getDisplayedSecondBuyItem().copy() 
                         /*?} else {*/
-                        offer.getSecondBuyItem().copy()
-                        /*?}*/;
+                        /*offer.getSecondBuyItem().copy()
+                        *//*?}*/;
                 var sell = offer.getSellItem().copy();
 
                 context.drawItem(firstBuy, baseX, baseY);
                 //? if >=1.21.3 {
-                /*context.drawStackOverlay(textRenderer, firstBuy, baseX, baseY);
-                *///?} elif <1.21.3 {
-                 context.drawItemInSlot(textRenderer, firstBuy, baseX, baseY);
-                //?}
+                context.drawStackOverlay(textRenderer, firstBuy, baseX, baseY);
+                //?} elif <1.21.3 {
+                 /*context.drawItemInSlot(textRenderer, firstBuy, baseX, baseY);
+                *///?}
 
                 context.drawItem(secondBuy, baseX + 20, baseY);
                 //? if >=1.21.3 {
-                /*context.drawStackOverlay(textRenderer, secondBuy, baseX + 20, baseY);
-                *///?} elif <1.21.3 {
-                 context.drawItemInSlot(textRenderer, secondBuy, baseX + 20, baseY);
-                //?}
+                context.drawStackOverlay(textRenderer, secondBuy, baseX + 20, baseY);
+                //?} elif <1.21.3 {
+                 /*context.drawItemInSlot(textRenderer, secondBuy, baseX + 20, baseY);
+                *///?}
 
                 context.drawItem(sell, baseX + 53, baseY);
                 //? if >=1.21.3 {
-                /*context.drawStackOverlay(textRenderer, sell, baseX + 53, baseY);
-                *///?} elif <1.21.3 {
-                 context.drawItemInSlot(textRenderer, sell, baseX + 53, baseY);
-                //?}
+                context.drawStackOverlay(textRenderer, sell, baseX + 53, baseY);
+                //?} elif <1.21.3 {
+                 /*context.drawItemInSlot(textRenderer, sell, baseX + 53, baseY);
+                *///?}
 
                 this.renderArrow(context, offer, baseX + -20, baseY);
 
                 List<String> enchantments = new ArrayList<>();
 
                 /*? if >= 1.20.6 {*/
-//                var itemEnchantmentsComponent = EnchantmentHelper.getEnchantments(offer.getSellItem());
-//                if (EnchantmentHelper.hasEnchantments(offer.getSellItem())) {
-//                    /*? if >=1.21 {*/
-//                     /*for (var entry : itemEnchantmentsComponent.getEnchantmentEntries()) {
-//                    *//*?} else {*/
-//                    for (var entry : itemEnchantmentsComponent.getEnchantmentsMap()) {
-//                    var enchantment = entry.getKey().value();
-//                    /*?}*/
-//                        var level = entry.getIntValue();
-//                        /*? if >=1.21 {*/
-//                        /*enchantments.add(Enchantment.getName(entry.getKey(), level).getString());
-//                        *//*?} else {*/
-//                         enchantments.add(enchantment.getName(level).getString());
-//                        /*?}*/
-//
-//                    }
-//                }
+                var itemEnchantmentsComponent = EnchantmentHelper.getEnchantments(offer.getSellItem());
+                if (EnchantmentHelper.hasEnchantments(offer.getSellItem())) {
+                    /*? if >=1.21 {*/
+                     for (var entry : itemEnchantmentsComponent.getEnchantmentEntries()) {
+                    /*?} else {*/
+                    /*for (var entry : itemEnchantmentsComponent.getEnchantmentsMap()) {
+                    var enchantment = entry.getKey().value();
+                    *//*?}*/
+                        var level = entry.getIntValue();
+                        /*? if >=1.21 {*/
+                        enchantments.add(Enchantment.getName(entry.getKey(), level).getString());
+                        /*?} else {*/
+                         /*enchantments.add(enchantment.getName(level).getString());
+                        *//*?}*/
+
+                    }
+                }
                 /*?} else {*/
-                var map = EnchantmentHelper.get(offer.getSellItem());
+                /*var map = EnchantmentHelper.get(offer.getSellItem());
                 for (var entry : map.entrySet()) {
                     var enchantment = entry.getKey();
                     var level = entry.getValue();
                     enchantments.add(enchantment.getName(level).getString());
                 }
-                /*?}*/
+                *//*?}*/
 
                 context.drawTextWithShadow(textRenderer, String.join(", ", enchantments), (baseX + 75), (baseY + 5),
                         0xFFFFFF);
@@ -145,25 +145,25 @@ public class OffersHUDRenderer implements HudRenderCallback {
     private void renderArrow(DrawContext context, TradeOffer tradeOffer, int x, int y) {
         if (tradeOffer.isDisabled()) {
             //? if >=1.21.3 {
-            /*context.drawTexture(
+            context.drawTexture(
                     RenderLayer::getGuiTextured, TEXTURE,
                     x + 5 + 35 + 20, y + 3,
                     25.0F, 171.0F,
                     10, 9,
                     512, 256);
-            *///?} elif <1.21.3 {
-             context.drawTexture(TEXTURE, x + 5 + 35 + 20, y + 3, 0, 25.0F, 171.0F, 10, 9, 512, 256);
-            //?}
+            //?} elif <1.21.3 {
+             /*context.drawTexture(TEXTURE, x + 5 + 35 + 20, y + 3, 0, 25.0F, 171.0F, 10, 9, 512, 256);
+            *///?}
         } else {
             //? if >=1.21.3 {
-            /*context.drawTexture(
+            context.drawTexture(
                     RenderLayer::getGuiTextured, TEXTURE,
                     x + 5 + 35 + 20, y + 3,
                     15.0F, 171.0F, 10, 9,
                     512, 256);
-            *///?} elif <1.21.3 {
-             context.drawTexture(TEXTURE, x + 5 + 35 + 20, y + 3, 0, 15.0F, 171.0F, 10, 9, 512, 256);
-            //?}
+            //?} elif <1.21.3 {
+             /*context.drawTexture(TEXTURE, x + 5 + 35 + 20, y + 3, 0, 15.0F, 171.0F, 10, 9, 512, 256);
+            *///?}
         }
     }
 }
